6a65b308e8625127a80acf94618dee32616c7419
npartovi <npartovi@gmail.com>
2018-10-24 15:58:19 -0700

    input done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      side1: null,
+      side2: null,
+      side3: null
+    }
+  }
+
+  onChange = (field) => {
+    return (e) => {
+      this.setState({[field]: e.target.value})
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <h1>Hello!!!!!!!!!!!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>side1</label>
+            <input onChange={this.onChange("side1")} type="text" />
+          <label>side2</label>
+            <input onChange={this.onChange("side2")} type="text" />
+          <label>side3</label>
+            <input onChange={this.onChange("side3")} type="text" />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
