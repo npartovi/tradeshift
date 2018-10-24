2f8ec225abc4d5841c53c8ae89876745686236c9
npartovi <npartovi@gmail.com>
2018-10-24 16:14:29 -0700

    validation on input done

--- src/App.js
+++ src/App.js
@@ -23,19 +23,25 @@ class App extends Component {
     console.log(this.state)
   }
 
-
+  submitEnabled = (side1, side2, side3) => {
+    return [side1,side2,side3].every((side) => !isNaN(side) && side > 0)
+  }
 
   render() {
+
+    const {side1, side2, side3} = this.state
+    const isEnabled = this.submitEnabled(side1,side2,side3)
+
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit}>
           <label>side1</label>
-            <input onChange={this.onChange("side1")} type="text" />
+            <input maxLength={1} onChange={this.onChange("side1")} type="text" />
           <label>side2</label>
-            <input onChange={this.onChange("side2")} type="text" />
+            <input maxLength={1} onChange={this.onChange("side2")} type="text" />
           <label>side3</label>
-            <input onChange={this.onChange("side3")} type="text" />
-          <button type="submit">Submit</button>
+            <input maxLength={1} onChange={this.onChange("side3")} type="text" />
+          <button disabled={!isEnabled} type="submit">Submit</button>
         </form>
       </div>
     );
