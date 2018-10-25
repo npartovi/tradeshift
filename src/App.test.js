7efdd9308198b5dc349a3db76e18a46fee54dad5
npartovi <npartovi@gmail.com>
2018-10-24 22:24:43 -0700

    tests are done

--- src/App.test.js
+++ src/App.test.js
@@ -10,11 +10,48 @@ it('renders without crashing', () => {
 });
 
 describe('input check', () => {
-  it("errors when wrong input is given", () => {
+   it("errors when number is less than zero", () => {
     const error1 = inputCheck(0)
-    const error2 = inputCheck("a")
+    expect(error1).toEqual(["Must enter a Value Greater Than 0"])
+   })
 
-    expect(error1).toEqual("Must Enter In a Numeric Value")
-    expect(error2).toEqual("Must enter a Value Greater Than 0")
+   it("errors when no numeric value is inputted", () => {
+    const error2 = inputCheck("a")
+    expect(error2).toEqual(["Must Enter In a Numeric Value"])
+   })
+})
+
+
+describe('determine type of triangle', () => {
+  it('it returns "Equilateral" triangle', () => {
+    let side1 = 3
+    let side2 = 3
+    let side3 = 3
+
+    const results = validateSides(side1, side2, side3)
+    expect(results).toEqual("Equilateral")
+  })
+
+  it('it returns "Scalene" triangle', () => {
+    let side1 = 1
+    let side2 = 2
+    let side3 = 3
+
+    const results = validateSides(side1, side2, side3)
+    expect(results).toEqual("Scalene")
+    
+  })
+
+  it('it returns "Isoceles" triangle', () => {
+    let side1 = 1
+    let side2 = 2
+    let side3 = 2
+
+    const results = validateSides(side1, side2, side3)
+    expect(results).toEqual("Isoceles")
+    
   })
 })
+
+
+
