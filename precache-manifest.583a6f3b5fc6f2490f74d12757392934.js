41512fd7af579af65ffa4862df64b3b9f8247e6f
npartovi <npartovi@gmail.com>
2018-10-25 14:42:31 -0700

    Updates

---
+++ precache-manifest.583a6f3b5fc6f2490f74d12757392934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e03b9cc03324f1fd4a5",
+    "url": "/tradeshift/static/js/runtime~main.3e03b9cc.js"
+  },
+  {
+    "revision": "a3c1bd6ec72a72e56a47",
+    "url": "/tradeshift/static/js/main.a3c1bd6e.chunk.js"
+  },
+  {
+    "revision": "eeb96a50a6cd5c5571f4",
+    "url": "/tradeshift/static/js/1.eeb96a50.chunk.js"
+  },
+  {
+    "revision": "a3c1bd6ec72a72e56a47",
+    "url": "/tradeshift/static/css/main.6efa550e.chunk.css"
+  },
+  {
+    "revision": "4dda92218d30865272a2979a1a01ae73",
+    "url": "/tradeshift/index.html"
+  }
+];
